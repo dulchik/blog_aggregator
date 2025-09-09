9e5a8e73edcfec8164652d75bad1bc9abaf940b7
dulchik <bubnyukab@gmail.com>
2025-09-09 13:40:21 +0200

    added users listing command

--- handler_user.go
+++ handler_user.go
@@ -54,6 +54,22 @@ func handlerLogin(s *state, cmd command) error {
 	return nil
 }
 
+func handlerListUsers(s *state, cmd command) error {
+	users, err := s.db.GetUsers(context.Background())
+	if err != nil {
+		return fmt.Errorf("couldn't list users: %w", err)
+	}
+
+	for _, user := range users {
+		if user == s.cfg.CurrentUserName {
+			fmt.Printf(" * %v (current)\n", user)
+			continue
+		}
+		fmt.Printf(" * %v\n", user)
+	}
+	return nil
+}
+
 func printUser(user database.User) {
 	fmt.Printf(" * ID:			%v\n", user.ID)
 	fmt.Printf(" * Name:		%v\n", user.Name)
--- main.go
+++ main.go
@@ -40,6 +40,7 @@ func main() {
 	c.register("login", handlerLogin)
 	c.register("register", handlerRegister)
 	c.register("reset", handlerReset)
+	c.register("users", handlerListUsers)
 
 	if len(os.Args) < 2 {
 		log.Fatal("Usage: cli <command> [args...]")
